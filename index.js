7846803f7cfdbe76aaebd9e6e61b43451238e11e
zolikobol <zoltan.kobol@globallogic.com>
2018-09-23 15:17:06 +0200

    trying

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
+const { DialogflowApp } = require('actions-on-google');
+
 const http = require('http');
 const PORT = process.env.PORT || 5000;
+
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+    const app = new DialogflowApp({ request: request, response: response });
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   console.log('=======================');
+//   console.log(req);
+//   console.log('=======================');
+    const welcomeIntent = (app) => {
+        app.ask('Hello how are you?');
+    }
+    const actionMap = new Map();
+    actionMap.set('input.welcome', welcomeIntent);
+    app.handleRequest(actionMap);
 });
 server.listen(PORT, () => {
   console.log(`Server running on ${PORT}/`);
